f798c1b2c3a7d6a106808c302051769248b7b609
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-10-20 19:15:50 +0200

    Copy wasm to public

--- src/clj/build.clj
+++ src/clj/build.clj
@@ -2,7 +2,18 @@
   (:require
    [clojure.java.shell :refer [sh]]))
 
-(defn remove-locate-wasm-locally-line
+(defn- copy-wasm-to-public
+  "Copies zenroom.wasm file to resources/public so that it is available in the
+  build.
+
+  Returns a map with `:exit` `:out` and `:err` keys from
+  `clojure.java.shell/sh`."
+  []
+  (sh "cp"
+      "node_modules/zenroom/dist/lib/zenroom.wasm"
+      "resources/public/"))
+
+(defn- remove-locate-wasm-locally-line
   "In the zenroom.js's node_module there's a line that tries to retrieve the wasm
   locally. This function removes that line so that the wasm is retrieved from
   resources/public instead.
@@ -16,6 +27,10 @@
 (defn fix-zenroom-wasm-path-hook
   {:shadow.build/stage :flush}
   [build-state]
+  (let [{:keys [exit err]} (copy-wasm-to-public)]
+    (if (zero? exit)
+      (println "Succesfully copied zenroom.wasm to public")
+      (println "Failed to copy zenroom.wasm to public - err: " err)))
   (let [{:keys [exit err]} (remove-locate-wasm-locally-line)]
     (if (zero? exit)
       (println "Succesfully changed zenroom.js's zenroom.wasm path")
