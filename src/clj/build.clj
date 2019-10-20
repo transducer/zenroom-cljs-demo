78245d257736dc51d85e7f004e7d44cdbe7f11b9
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-10-20 19:32:43 +0200

    Extract call-and-log-sh

--- src/clj/build.clj
+++ src/clj/build.clj
@@ -1,38 +1,41 @@
 (ns build
   (:require
-   [clojure.java.shell :refer [sh]]))
+   [clojure.java.shell :refer [sh]]
+   [clojure.string :as string]))
+
+(defn- ppstring-fn-name
+  "Pretty printed string of name of fn"
+  [f]
+  (let [[_ name _] (string/split (str f) #"\$|@")]
+    (string/replace name "_" " ")))
+
+(defn- call-and-log-sh [sh-fn]
+  (let [{:keys [exit err]} (sh-fn)]
+    (if (zero? exit)
+      (println "Succesfully run" (ppstring-fn-name sh-fn))
+      (println "Failed to run " (ppstring-fn-name sh-fn) " - err: " err))))
 
 (defn- copy-wasm-to-public
   "Copies zenroom.wasm file to resources/public so that it is available in the
-  build.
-
-  Returns a map with `:exit` `:out` and `:err` keys from
-  `clojure.java.shell/sh`."
+  build."
   []
-  (sh "cp"
-      "node_modules/zenroom/dist/lib/zenroom.wasm"
-      "resources/public/"))
+  (call-and-log-sh
+   #(sh "cp"
+        "node_modules/zenroom/dist/lib/zenroom.wasm"
+        "resources/public/")))
 
 (defn- remove-locate-wasm-locally-line
   "In the zenroom.js's node_module there's a line that tries to retrieve the wasm
   locally. This function removes that line so that the wasm is retrieved from
-  resources/public instead.
-
-  Returns a map with `:exit` `:out` and `:err` keys from
-  `clojure.java.shell/sh`."
+  resources/public instead."
   []
-  (sh "sed" "-i.bak" "/wasmBinaryFile = locateFile/d"
-      "node_modules/zenroom/dist/lib/zenroom.js"))
+  (call-and-log-sh
+   #(sh "sed" "-i.bak" "/wasmBinaryFile = locateFile/d"
+        "node_modules/zenroom/dist/lib/zenroom.js")))
 
 (defn fix-zenroom-wasm-path-hook
   {:shadow.build/stage :flush}
   [build-state]
-  (let [{:keys [exit err]} (copy-wasm-to-public)]
-    (if (zero? exit)
-      (println "Succesfully copied zenroom.wasm to public")
-      (println "Failed to copy zenroom.wasm to public - err: " err)))
-  (let [{:keys [exit err]} (remove-locate-wasm-locally-line)]
-    (if (zero? exit)
-      (println "Succesfully changed zenroom.js's zenroom.wasm path")
-      (println "Failed to change zenroom.js's zenroom.wasm path - err: " err)))
+  (copy-wasm-to-public)
+  (remove-locate-wasm-locally-line)
   build-state)
