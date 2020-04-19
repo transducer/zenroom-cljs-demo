916cd062271e2582102845500686f3373b22a24f
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-04-19 23:07:13 +0200

    Bump versions

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject org.dyne.zenroom-example "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"
+                 [org.clojure/clojurescript "1.10.597"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.62"]
-                 [reagent "0.8.1"]
-                 [re-frame "0.10.9"]]
+                 [thheller/shadow-cljs "2.8.98"]
+                 [reagent "0.10.0"]
+                 [re-frame "0.12.0"]]
   :plugins []
   :min-lein-version "2.5.3"
   :jvm-opts ["-Xmx1G"]
@@ -13,7 +13,5 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :aliases {"dev"  ["with-profile" "dev" "run" "-m" "shadow.cljs.devtools.cli" "watch" "app"]
             "prod" ["with-profile" "prod" "run" "-m" "shadow.cljs.devtools.cli" "release" "app"]}
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.10"]]}
-   :prod {}})
+  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.0"]]}
+             :prod {}})
