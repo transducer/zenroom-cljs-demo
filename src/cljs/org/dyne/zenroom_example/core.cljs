b5eaa440e64151552fef28bf2516291f6e9eba51
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-04-30 23:27:18 +0200

    Style things a bit

--- src/cljs/org/dyne/zenroom_example/core.cljs
+++ src/cljs/org/dyne/zenroom_example/core.cljs
@@ -1,34 +1,43 @@
 (ns org.dyne.zenroom-example.core
   (:require
+   [cljs.reader :refer [read-string]]
    [clojure.string :as string]
    [reagent.core :as r]
    [reagent.dom :as dom]
    [zenroom :default zenroom]))
 
-(defn main-panel []
+(defn panel []
   (let [input (r/atom "")
         KEYS (r/atom "")
         DATA (r/atom "")
-        success? (r/atom true)
+        success? (r/atom false)
         results (r/atom [])
         evaluate! (fn []
+                    (reset! results [])
                     (doto zenroom
                       (.script @input)
-                      (.keys @KEYS)
-                      (.data @DATA)
+                      (.keys (clj->js (read-string @KEYS)))
+                      (.data (clj->js (read-string @DATA)))
                       (.print (fn [s] (swap! results conj s)))
                       (.success (fn [] (reset! success? true)))
                       (.error (fn [] (reset! success? false)))
                       .zencode-exec))]
     (fn []
-      [:<>
-       [:textarea {:on-change (fn [e] (reset! input (-> e .-target .-value)))}]
-       [:button {:on-click evaluate!} "Evaluate"]
-       [:textarea {:value (string/join "\n" @results) :read-only true}]
-       [:textarea {:on-change (fn [e] (reset! KEYS (-> e .-target .-value)))}]
-       [:textarea {:on-change (fn [e] (reset! DATA (-> e .-target .-value)))}]
-       [:div "Compiles?"]
-       [:div {:style {:background-color (if @success? "green" "red") :height "30px" :width "30px"}}]])))
+      [:div.container
+       [:h1.title.is-1 "Zenroom Demo"]
+       [:div.columns
+        [:div.column
+         [:h2.subtitle.is-2 "Code"
+          [:textarea.textarea {:rows 20 :on-change (fn [e] (reset! input (-> e .-target .-value)))}]
+          [:button.button {:on-click evaluate!} "Evaluate"]]
+         [:label "Compiles? " [:input {:type :checkbox :checked @success? :read-only true}]]]
+        [:div.column
+         [:h2.subtitle.is-2 "Keys"
+          [:textarea.textarea {:rows 3 :on-change (fn [e] (reset! KEYS (-> e .-target .-value)))}]]
+         [:h2.subtitle.is-2 "Data"
+          [:textarea.textarea {:rows 3 :on-change (fn [e] (reset! DATA (-> e .-target .-value)))}]]
+         [:h2.subtitle.is-2 "Result"
+          [:textarea.textarea {:rows 10 :value (string/join "\n" @results) :read-only true}]]]]])))
 
 (defn ^:export ^:dev/after-load init []
-  (dom/render [main-panel] (.getElementById js/document "app")))
+  (dom/render [panel] (.getElementById js/document "app")))
